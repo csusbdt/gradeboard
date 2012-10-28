f2e4a9c3fccc99d1b8f0a8a4306ff90c2f803603
turner <csusbdt@gmail.com>
2012-10-27 17:18:23 -0700

    Added files from project creation wizard.

---
+++ gradesys/src/gradesys/GradesysServlet.java
@@ -0,0 +1,11 @@
+package gradesys;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class GradesysServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
